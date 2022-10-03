76c0bea547fa83ef7983c6eecea6607dc9932bdf
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2022-10-03 22:34:30 +0200

    Update Project

--- src/Components/SectionCathegory/SectionCathegory.jsx
+++ src/Components/SectionCathegory/SectionCathegory.jsx
@@ -6,17 +6,39 @@ export const SectionCathegory = () => {
   return (
     <>
       <div className='contain'>
-        <select id='option-category-default'>
-          <option value='Domyślnie'>Domyślnie</option>
-          <option value='saab'>Saab</option>
-          <option value='opel'>Opel</option>
-          <option value='audi'>Audi</option>
-        </select>
+        <div className='sort'>
+          <select id='option-category-default'>
+            <option value='Domyślnie'>Domyślnie</option>
+            <option value='saab'>Saab</option>
+            <option value='opel'>Opel</option>
+            <option value='audi'>Audi</option>
+          </select>
+        </div>
       </div>
       <div className='contain'>
         <div className='row'>
           <div className='col-sm-3'>
-            <p>test</p>
+            <h3>Producent</h3>
+            <div>
+              <input type='checkbox' id='vehicle1' name='vehicle1' value='Bike' />
+              <label htmlFor='vehicle1'>Producent (ilość prod.)</label>
+              <br />
+              <input type='checkbox' id='vehicle2' name='vehicle2' value='Car' />
+              <label htmlFor='vehicle2'>Producent (ilość prod.)</label>
+              <br />
+              <input type='checkbox' id='vehicle3' name='vehicle3' value='Boat' />
+              <label htmlFor='vehicle3'>Producent (ilość prod.)</label>
+              <br />
+              <input type='checkbox' id='vehicle3' name='vehicle3' value='Boat' />
+              <label htmlFor='vehicle3'>Producent (ilość prod.)</label>
+              <br />
+              <input type='checkbox' id='vehicle3' name='vehicle3' value='Boat' />
+              <label htmlFor='vehicle3'>Producent (ilość prod.)</label>
+              <br />
+              <input type='checkbox' id='vehicle3' name='vehicle3' value='Boat' />
+              <label htmlFor='vehicle3'>Producent (ilość prod.)</label>
+              <br />
+            </div>
           </div>
           <div className='col-sm-9'>
             <div className='row'>
