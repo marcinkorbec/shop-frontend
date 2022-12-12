443ecb690c7a3860c26d0ad5c491175b180e0e3d
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2022-12-12 20:31:51 +0100

    Update Homepage

--- src/Components/ProductCardHomepage/ProductCard.jsx
+++ src/Components/ProductCardHomepage/ProductCard.jsx
@@ -2,6 +2,7 @@ import Card from 'react-bootstrap/Card';
 import './ProductCardHomepage.css';
 
 export const ProductCard = (width) => {
+  const price = 1119;
   return (
     <Card style={width}>
       <div className='card-img-container'>
@@ -10,7 +11,8 @@ export const ProductCard = (width) => {
       <Card.Body>
         <Card.Title>Długa dwuliniowa nazwa produktu</Card.Title>
         <Card.Text>
-          <span id='card-price'>1 119,00</span>&nbsp; <span id='zl'>zł</span>
+          <span id='card-price'>{price.toLocaleString('pl-PL', { minimumFractionDigits: 2 })}</span>
+          &nbsp; <span id='zl'>zł</span>
         </Card.Text>
       </Card.Body>
     </Card>
